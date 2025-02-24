4facf7b10bc82dbdd0380f790d750d71a3550664
Ajo Alex <67946056+devpenzil@users.noreply.github.com>
2025-02-24 14:41:26 +0530

    package migration

--- Sources/AjoChatView/AjoChatView.swift
+++ Sources/AjoChatView/AjoChatView.swift
@@ -1,10 +1,35 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import UIKit
 
-class AjoChatView{
-    init(){
-        print("Hello")
+class ChatView:UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource{
+    var messages : [Message] = []
+    
+    init(frame: CGRect) {
+            let layout = UICollectionViewFlowLayout()
+            layout.scrollDirection = .vertical
+            layout.estimatedItemSize = CGSize(width: frame.width, height: 50)
+            
+            super.init(frame: frame, collectionViewLayout: layout)
+            self.backgroundColor = .white
+            self.delegate = self
+            self.dataSource = self
+            self.register(SingleMessage.self, forCellWithReuseIdentifier: "cell")
+        }
+        
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
     }
-   
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SingleMessage
+               cell.configure(with: messages[indexPath.item])
+        return cell
+    }
 }
